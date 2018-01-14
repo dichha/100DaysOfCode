9575afe15752df08f60f948b653f30af96b8394f
Dichha Rai <dichha.chamling@gmail.com>
2018-01-13 23:01:25 -0600

    day 15: Stack Min -- a stack w/ push & pop also return min element in O(1) time. a) have each node keep track of min value as well.

---
+++ src/algorithms/stacksandqueues/StackWithMin.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms.stacksandqueues;
+
+import java.util.Stack; 
+/**
+ *
+ * @author dichha
+ */
+public class StackWithMin extends Stack<NodeWithMin>{
+    public void push(int value){
+        int newMin = Math.min(value, min());
+        super.push(new NodeWithMin(value, newMin));
+    }
+    
+    public int min(){
+        if(this.isEmpty())
+            return Integer.MAX_VALUE; 
+        else
+            return peek().min; 
+    }
+    
+   
+}
+class NodeWithMin{
+       public int value; 
+       public int min; 
+       public NodeWithMin(int v, int min){
+           value = v; 
+           this.min = min; 
+       }
+   }
