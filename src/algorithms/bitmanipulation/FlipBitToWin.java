4af18350c4d6d92c8983dd06031c0e2c9b107f54
Dichha Rai <dichha.chamling@gmail.com>
2018-01-30 15:20:23 -0600

    day 32: FlipBitToWin: O(n^2), flip each one and count max # of 1s, Optimal: Count # of 1s & 0s. which will have pattern of x0s, y1s. iterate over it to find max # on concatenating 2 adjacent nums.

--- src/algorithms/bitmanipulation/FlipBitToWin.java
+++ src/algorithms/bitmanipulation/FlipBitToWin.java
@@ -5,10 +5,96 @@
  */
 package algorithms.bitmanipulation;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author dichha
  */
 public class FlipBitToWin {
+    static int SEQUENCE_LENGTH = 32; 
+    static int longestSequence(int n){
+        int max = 0; 
+        for(int i=0; i<SEQUENCE_LENGTH; i++){
+            max = Math.max(max, longestSequenceOf1s(n,i));
+        }
+        return max; 
+    }
+    static int longestSequenceOf1s(int n, int idxToIgnore){
+        int counter=0;
+        int max = 0; 
+        for (int j=0; j< SEQUENCE_LENGTH; j++){
+            if(j == idxToIgnore || getBit(n, j)){
+                counter++;  
+                
+            }else{
+                counter=0;
+            }
+            max = Math.max(max, counter);
+        }
+        return max; 
+        
+    }
+    static boolean getBit(int n, int idx){
+        return (n & (1 << idx)) != 0; 
+    }
+    /*
+    public static void main(String[] args){
+        int n = 1775;
+        int count = longestSequence(n);
+        System.out.print(count);
+        
+        
+    }
+    */
     
+    //Improved Solution
+    int longestSequence(int n){
+        
+        if (n == -1) return SEQUENCE_LENGTH; // special case of no zeros at all. 
+        
+        ArrayList<Integer> sequences = getAlternatingSequences(n); 
+        return findLongestSequence(sequences); 
+        
+    }
+    // Return list of the sequence sizes. The sequence starts offf with the number of 0s
+    // which might be (0) and then alternates with the counts of each values
+    
+    ArrayList<Integer> getAlternatingSequences(int n){
+        ArrayList<Integer> sequences = new ArrayList<Integer>(); 
+        int searchingFor = 0; 
+        int counter = 0; 
+        
+        for (int i=0; i < SEQUENCE_LENGTH; i++){ 
+            if ((n & 1) != searchingFor){
+                sequences.add(counter); 
+                searchingFor = n & 1; // Flip 1 to 0 or 1 
+                counter = 0; 
+            }
+             counter++; 
+            n >>>= 1; // We want to shift the sign bit too. 
+        }
+        sequences.add(counter); 
+        return sequences;
+    }
+    int findLongestSequence(ArrayList<Integer> seq){
+        int maxSeq = 1; 
+        for (int i = 0; i < seq.size(); i += 2){
+            int zerosSeq = 1; 
+            int onesSeqRight = i - 1 >= 0 ? seq.get(i-1) : 0; 
+            int onesSeqLeft = i + 1 < seq.size() ? seq.get(i+1): 0;
+            
+            int thisSeq = 0; 
+            if (zerosSeq == 1){// can merge
+                thisSeq = onesSeqLeft + 1 + onesSeqRight; 
+            }else if(zerosSeq > 1){
+                thisSeq = 1 + Math.max(onesSeqRight, onesSeqLeft);
+            }else if (zerosSeq == 0){ // no zeros but take either side
+                thisSeq = Math.max(onesSeqRight, onesSeqLeft); 
+            }
+            maxSeq = Math.max(thisSeq, maxSeq); 
+        }
+        return maxSeq; 
+    }
+     
 }
