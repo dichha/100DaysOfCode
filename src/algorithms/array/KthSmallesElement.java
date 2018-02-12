c48c601ddca5a7ae4bbf54fff68bdbf70a576240
dichha <dichha@192.168.0.13>
2018-02-12 00:18:01 -0600

    day 44: Find Kth smallest element O(n) using quick sort. if pivot == pos, then return arr[pivot], else will check left most part, if < than kth go to recurse to the right.

--- src/algorithms/array/KthSmallesElement.java
+++ src/algorithms/array/KthSmallesElement.java
@@ -10,6 +10,7 @@ package algorithms.array;
  * @author dichha
  */
 public class KthSmallesElement {
+    /*
     static int kthSmallest(int[] arr, int k){
         for(int i=0; i < arr.length; i++){ 
             for(int j=1; j < arr.length-i; j++){
@@ -21,10 +22,50 @@ public class KthSmallesElement {
             }
         }
         return arr[k-1]; 
+    }*/
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a]; 
+        arr[a] = arr[b]; 
+        arr[b] = temp; 
     }
+    static int partition(int[] arr, int l, int r){
+        int x = arr[r]; 
+        int i = l; 
+        for(int j = l; j <= r-1; j++){
+            if (arr[j] <= x){
+                swap(arr, i, j);
+                i++; 
+            }
+        }
+        swap(arr, i, r); 
+        return i; 
+    }
+    
+    static int kthSmallest(int[] arr, int l, int r, int k){
+        // if k is smaller than number of elements in array
+        if(k > 0 && k <= r-l+1){
+            // partiion the array around last 
+            // element and get position of pivot
+            // element in sorted array
+            int pos = partition(arr, l, r);
+            if(pos-1 == k-1)
+                return arr[pos]; 
+            
+            // if position is more , recur for left subarray
+            if ((pos -1) > (k-1))
+                return kthSmallest(arr, l, pos-1, k); 
+            
+            // Else recur for right subarray
+            return kthSmallest(arr, pos+1, r, k-pos+l-1);
+            
+            
+        }
+        return Integer.MAX_VALUE;
+    }
+    
     public static void main(String[] args){
         int[] arr = {7, 10, 4, 3, 20, 15}; 
-        int n = kthSmallest(arr, 3);
+        int n = kthSmallest(arr, 0, arr.length-1, 3);
         System.out.print(n);
     }
     
