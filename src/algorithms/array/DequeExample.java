51b8c044dfa506985beb40f3de8bb4efd1401821
dichha <dichha@192.168.0.13>
2018-03-01 23:24:36 -0600

    day 60: Deque methods

---
+++ src/algorithms/array/DequeExample.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms.array;
+
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author dichha
+ */
+public class DequeExample {
+    /*
+    public static void main(String[] args){
+        Deque<String> deque = new LinkedList<>(); 
+        
+        // add methods 
+        System.out.println("ADD Operations..."); 
+        deque.add("Tail-1"); 
+        deque.addLast("Tail-2"); 
+        deque.offer("Tail-3"); 
+        deque.offerLast("Tail-4"); 
+        
+        deque.addFirst("Head-1");
+        deque.offerFirst("Head-2"); 
+        deque.offerFirst("Head-3");
+        
+        System.out.println("Deque is: " + deque); 
+        
+        // Iterations 
+        
+        System.out.println("ITERATOR......");
+        Iterator<String> iterator = deque.iterator(); 
+      
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        
+        // peek 
+        System.out.println("Peek: " + deque.peek());
+        System.out.println("Peek First: " + deque.peekFirst());
+        System.out.println("Peek Last: " + deque.peekLast()); 
+        
+        // push pop -- act lik stack
+        
+        System.out.println("Stack operations: Push"); 
+        deque.push("Head-4");
+        System.out.println("Deque; " + deque); 
+        System.out.println("Stack operations: Pop"); 
+        deque.pop(); 
+        
+        
+        // remove operations 
+        System.out.println("REMOVE Operations...");
+        deque.remove(); 
+        deque.removeFirst(); 
+        deque.removeLast(); 
+        
+        System.out.println("Deque: " + deque);
+       
+        // 
+        
+        
+        
+                
+        
+        
+    }
+    */
+    
+}
