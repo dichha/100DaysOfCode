d5fae46c36e333ce2687a2260443303ae09abe9e
dichha <dichha@192.168.0.13>
2018-03-11 04:22:01 -0500

    day 67: Bit Manipulation revision, set, get, update, clear, logical vs arithmetic shift

--- src/algorithms/bitmanipulation/BitsBasicRevision.java
+++ src/algorithms/bitmanipulation/BitsBasicRevision.java
@@ -38,6 +38,6 @@ public class BitsBasicRevision {
     int updateBit(int num, int i, boolean bitIs1){
         int value = bitIs1 ? 1 : 0; 
         int mask = ~(1 << i);
-        return (num & mask) | (value << i); 
+        return ((num & mask) | (value << i)); 
     }
 }
