cff5187a664e99ce938a075e7c5d3d30f7fbc9ba
Dichha Rai <dichha.chamling@gmail.com>
2018-03-21 00:13:20 -0500

    day 76: traverse disconnected graph in DFS way

--- src/algorithms/treesandgraphs/DFSDisconectedGraph.java
+++ src/algorithms/treesandgraphs/DFSDisconectedGraph.java
@@ -30,8 +30,36 @@ public class DFSDisconectedGraph {
             boolean[] visited = new boolean[vertices]; 
             System.out.println("DFS");; 
             
-            // visit for each node if not already visited. 
+            // visit for each node if not already visited. ]
+            for(int i=0; i<vertices; i++){
+                if(!visited[i]){
+                    dfs(i, visited); 
+                }
+            }
+        }
+        public void dfs(int start, boolean[] visited){
+            visited[start] = true;
+            System.out.println(start + " ");
+            for (int i=0; i<adjList[start].size(); i++){
+                int vertex = adjList[start].get(i);
+                if(!visited[vertex]){
+                    dfs(vertex, visited);
+                }
+            }
         }
         
     }
+    /*
+      public static void main(String[] args) {
+        int vertices = 7;
+        Graph graph = new Graph(vertices);
+        graph.addEdge(1,3);
+        graph.addEdge(2,3);
+        graph.addEdge(0,4);
+        graph.addEdge(4,5);
+        graph.addEdge(5,6);
+        graph.DFSRecursion();
+    }
+*/
+    
 }
