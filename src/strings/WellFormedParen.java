35e02c3c56efd298e22892c012378df59f17454f
Dichha Rai <dichha.chamling@gmail.com>
2018-04-12 00:14:21 -0500

    day 94: Well Formed paren, count for open & close paren, if close paren > close -> false.after iterating over all char check if open == close.

--- src/strings/WellFormedParen.java
+++ src/strings/WellFormedParen.java
@@ -10,5 +10,35 @@ package strings;
  * @author dichha
  */
 public class WellFormedParen {
-    
+    static boolean isWellFormed(String s){
+        int closeParen = 0; 
+        int openParen = 0; 
+        char[] arr = s.toCharArray(); 
+        for(char c: arr){
+            if(c == '{')
+                openParen ++; 
+            else if (c == '}')
+                closeParen ++; 
+            if(closeParen > openParen)
+                return false; 
+            
+        }
+        if(openParen == closeParen)
+            return true; 
+        else
+            return false; 
+         
+    }
+    public static void main(String[] args){
+        String x1 = "{{{{}}}}{}{}{}{}{}{}{}{}{}{}{{{}}}"; 
+        String x2 = "{}{"; 
+        String x3 = "}{"; 
+        
+        boolean ans = isWellFormed(x1);
+        System.out.println(ans); 
+        boolean ans2 = isWellFormed(x2);
+        System.out.println(ans2);
+        boolean ans3 = isWellFormed(x3);
+        System.out.println(ans3);
+    }
 }
